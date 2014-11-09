2597b36764bcaef7967e590dd5902e1d1327e7f0
neufmanu <manu.neufeld@gmail.com>
2014-11-09 13:48:07 +0100

    ajax on trohpy (dummy)

--- Loginpage/Schnürchen - trophy/js/hover.js
+++ Loginpage/Schnürchen - trophy/js/hover.js
@@ -1,47 +1,40 @@
-
 $(document).ready(function(){
-
     $(".con").hide();
-
-    $("#1").each(function(){
-
-        var trophy = this;
-        var con = $("#"+trophy).children('.con');
-
-        $("#"+trophy).hover(
-
+    $(['button2', 'button3', 'btn3']).each(function(){
+        var btn = this;
+        var con = $("#"+btn).children('.con');
+        $("#"+btn).hover(
             function(){
-
-                $(".hover").mouseout();
                 $(this).addClass('hover');
-                var cache = $(con).children('p');
-
+               var cache = $(con).children('img');
+                //check to see if content was loaded previously
                 if(cache.size()){
-                    con.show();
-                }else{
-                    $(con).show();
-                    $(con).html('<img src="/img/loader.gif" alt="Loading..." />');
-                    (function() {
-                        var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-                        $.getJSON( flickerAPI, {
-                            tags: "mount rainier",
-                            tagmode: "any",
-                            format: "json"
-                        })
-                            .done(function( data ) {
-                              $(con).html(data);
-                            });
-                    })();
-                }
-            },
-
-            function(){
-                if($.browser.msie){
-                    $(con).hide();
+                   con.show();
                 }
                 else {
-                    $(con).fadeOut("slow");
+                    $(con).show();
+                    //$(con).html('<img src="/img/loader.gif" alt="Loading..." />');
+                    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+                    $.getJSON( flickerAPI, {
+                        tags: "puppy",
+                        tagmode: "any",
+                        format: "json"
+                    }).done(function( data ) {
+                        $.each( data.items, function( i, item ) {
+                            $( "<img>" ).attr( "src", item.media.m ).appendTo( con );
+                            if ( i === 0 ) {
+                                return false;
+                            }
+                        });
+                    });
                 }
+            },
+            //mouseout
+            function(){
+
+                    $(con).fadeOut(250);
+
+
                 $(this).removeClass('hover');
             }
         );
