5833c5709f7dccedce8df2eb51e2d29af5c2c95d
neufmanu <manu.neufeld@gmail.com>
2014-11-13 20:09:45 +0100

    erste versuche in der aufteilung mit .erb's
    hmm lecker

--- app/app.rb
+++ app/app.rb
@@ -18,10 +18,19 @@ class App < Sinatra::Base
 		erb :games
 	end
 
+<<<<<<< Updated upstream
 	get "/game/dummy" do
 		@title = "Dummy Game"
 		@game_path = "#{settings.game_dir}" + "dummy_game.js"
 		erb :game
+=======
+	get "/home" do
+		@user = {
+        :name => "hans",
+        :age => 5
+    }
+		erb :home
+>>>>>>> Stashed changes
 	end
 
 	get "/userinsert" do
