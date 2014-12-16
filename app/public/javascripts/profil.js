326a86603a49d250ee2ccb7c94a60182ab6f62b7
neufmanu <manu.neufeld@gmail.com>
2014-12-16 18:31:13 +0100

    stylechanges in profil -> friendlist

--- app/public/javascripts/profil.js
+++ app/public/javascripts/profil.js
@@ -11,13 +11,15 @@ var friendProfil = $("#friendProfil");
 /* Freundessuche */
 
 var input = $("#friendsearchinput");
-var responseDiv = $("#responseDiv");
+var responseList = $("#responseList");
+var friendResponse = $("#friendResponse");
 
 function searchFriend(){
     var query = input.val();
 
     if(query.length==0){
-        responseDiv.html("");
+        responseList.html("");
+        friendResponse.hide();
     }
 
     $.get(
@@ -27,10 +29,10 @@ function searchFriend(){
 
             for(i=0;i<msg.length;i++){
                 var str =  msg[i]["username"];
-                resultString = resultString + "<li><b>"+ str.substring(0,query.length) + "</b>" + str.substring(query.length,str.length)+ "</li>"
+                resultString = resultString + "<li class='shownFriends'><b>"+ str.substring(0,query.length) + "</b>" + str.substring(query.length,str.length)+ "</li>";
             }
-            responseDiv.html(resultString)
-
+            friendResponse.show();
+            responseList.html(resultString);
         }
     );
 };
