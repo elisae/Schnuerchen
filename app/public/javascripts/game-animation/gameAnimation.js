824327d1ac1395540e7d794a69787aa9885443bd
neufmanu <manu.neufeld@gmail.com>
2014-12-16 13:31:46 +0100

    Kleine style Änderungen

--- app/public/javascripts/game-animation/gameAnimation.js
+++ app/public/javascripts/game-animation/gameAnimation.js
@@ -1,71 +1,13 @@
 /**
  * Created by manuelneufeld on 17/11/14.
  */
-$(document).ready(function(){
-/*
-   var leave = $(".end");
-   var game_div = $("#game_div");
-   var selection = $("#selection");
-   var start_div = $("#start_div");
-
-    $(leave).hide();
-    $(game_div).hide();
-    $(selection).hide();
-
-    $(".start").click(function(){
-       $(start_div).hide();
-       $(game_div).slideDown("slow");
-       $(game_div).css("background", "white");
-       $(".end").fadeIn("slow");
-    });
-
-    $(leave).click(function(){
-       $(game_div).fadeOut("slow", function(){
-           $(start_div).fadeIn("slow");
-       })
-    });
-    */
-
-    var start_div = $("#start_div");
-    var pause_div = $("#pause_div");
-    var game_div = $("#game_div");
-    var end_game_div = $("#end_game_div");
-
-
-
-    $("#button_start").click(function(){
-       $(pause_div).hide();
-       $(start_div).slideUp("slow");
-    });
-/*
-    $("#button_pause").click(function(){
-        $(pause_div).fadeIn("1500");
-    });
-
-    $("#button_continue").click(function(){
-        $(pause_div).fadeOut("fast");
-    });
-*/
-    $("#button_to_start").click(function(){
-        $(start_div).slideDown("slow");
-
-    });
-
-    $("#button_leave").click(function(){
-       //game_div.hide();
-       end_game_div.show();
-    });
-
-    //set_visibility('end_game_div', true);
-
-});
 
 var answerResponse = $("#answerResponse");
 
 answerResponse.hide();
 
 function feedbackRight(){
-    answerResponse.css("backgroundColor", "green");
+    answerResponse.css("backgroundColor", "#00E676");
     answerResponse.fadeIn("fast", function(){
         $(this).fadeOut("fast");
     });
@@ -73,9 +15,37 @@ function feedbackRight(){
 }
 
 function feedbackWrong(){
-    answerResponse.css("backgroundColor", "red");
+    answerResponse.css("backgroundColor", "#FF5252");
     answerResponse.fadeIn("fast", function(){
        $(this).fadeOut("fast");
     });
     console.log("falsche antwort");
 }
+
+
+
+$(document).ready(function(){
+
+    var start_div = $("#start_div");
+    var pause_div = $("#pause_div");
+    var game_div = $("#game_div");
+    var end_game_div = $("#end_game_div");
+
+    $("html, body").animate({ scrollTop: $("#game").scrollTop() }, 500);
+
+    $("#button_start").click(function(){
+        $(pause_div).hide();
+        $(start_div).slideUp("slow");
+    });
+
+    $("#button_to_start").click(function(){
+        $(start_div).slideDown("slow");
+
+    });
+
+    $("#button_leave").click(function(){
+        //game_div.hide();
+        end_game_div.show();
+    });
+
+});
