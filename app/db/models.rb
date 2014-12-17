948f6bdbd9f8aafc805f7ae162d390b8a9e8cd39
elisae <ee014@hdm-stuttgart.de>
2014-12-17 13:04:30 +0100

    Friends Abfrage foreal

--- app/db/models.rb
+++ app/db/models.rb
@@ -14,7 +14,7 @@ Sequel::Model.plugin :json_serializer
 
 # - INIT -----------------------------------------
 
-DB.drop_table?(:users_trophies, :trophies_users, :trophies, :scores, :games, :scoretypes, :gameranges_gametypes, :gametypes, :gameranges_operators, :gameranges, :operators, :users, :friends)
+DB.drop_table?(:users_trophies, :friendships, :trophies_users, :trophies, :scores, :games, :scoretypes, :gameranges_gametypes, :gametypes, :gameranges_operators, :gameranges, :operators, :users, :friends)
 
 
 # - USERS ---------------------------------------
@@ -28,8 +28,10 @@ unless DB.table_exists?(:users)
 	end
 end
 class User < Sequel::Model(:users)
-	many_to_many :trophies, :key => :trophy_id
-	one_to_many :friends
+	many_to_many :trophies
+
+	many_to_many :friends_with, :left_key=>:friends_with_id, :right_key=>:friend_of_id, :join_table=>:friendships, :class=>self
+  	many_to_many :friend_of, :left_key=>:friend_of_id, :right_key=>:friends_with_id, :join_table=>:friendships, :class=>self
 
 	def self.create(values = {}, &block)
 		puts "New User: #{values[:username]}"
@@ -50,15 +52,20 @@ end
 #   one_to_many :friend_id
 # end
 
-unless DB.table_exists?(:friends)
-	DB.create_table(:friends) do
+unless DB.table_exists?(:friendships)
+	DB.create_table(:friendships) do
 		primary_key :id
-		foreign_key :user_id
+		foreign_key :friends_with_id
+		foreign_key :friend_of_id
 	end
 end
 
-class Friend < Sequel::Model(:friends)
-	many_to_one :user
+class Friendship < Sequel::Model(:friendships)
+
+	def self.create(values = {}, &block)
+		puts "New Friendship: #{values[:friends_with_id]} -> #{values[:friend_of_id]}"
+		super
+	end
 end
 
 
@@ -291,13 +298,33 @@ User.create(:username=>"kennmer", :firstname=>"kenny", :email=>"kenny@kenny.de",
 # Friend.create(:user_id=>"1", :friend_id=>"4")
 # Friend.create(:user_id=>"2", :friend_id=>"3")
 
-User.find(:id => 1).add_friend(Friend.create(:user_id => 2))
-User.find(:id => 1).add_friend(Friend.create(:user_id => 3))
-User.find(:id => 1).add_friend(Friend.create(:user_id => 4))
-User.find(:id => 2).add_friend(Friend.create(:user_id => 1))
-User.find(:id => 3).add_friend(Friend.create(:user_id => 1))
+Friendship.create(:friends_with_id => 1, :friend_of_id => 2)
+Friendship.create(:friends_with_id => 2, :friend_of_id => 1)
 
-puts User.find(:id => 1).friends
+Friendship.create(:friends_with_id => 1, :friend_of_id => 3)
+Friendship.create(:friends_with_id => 1, :friend_of_id => 4)
+Friendship.create(:friends_with_id => 5, :friend_of_id => 1)
+
+friends = User.find(:id => 1).friends_with.map { |f|
+	f.to_hash
+} 
+
+puts "User 1 is friends_with"
+puts friends
+
+friends = User.find(:id => 1).friend_of.map { |f|
+	f.to_hash
+} 
+
+puts "User 1 is friend_of"
+puts friends
+
+puts friends?(1, 2)
+
+puts friends?(1, 3)
+
+puts friends?(1, 5)
+
 
 Gamerange.create(:name=>"10", :long_descr=> "Rechne mit den Zahlen von 1-10!")
 Gamerange.create(:name=>"20", :long_descr=> "Rechne mit den Zahlen von 1-20!")
