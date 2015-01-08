13526bcdb6920b4924449d5143ec232a5c6767af
wminet <wm007@hdm-stuttgart.de>
2015-01-08 13:55:21 +0100

    game_marathon_addi_100 angepasst mit zeitbalken und score

--- public/javascripts/games/game_marathon_addi_100.js
+++ public/javascripts/games/game_marathon_addi_100.js
@@ -39,7 +39,12 @@ var score_right = 10;               //Points for a right answer
 var score_wrong = 5;                //Points for a wrong answer
 var score_time_influence = 7000;    //score-formula: counter_right * score_right - counter_wrong * score_wrong - time_needed/score_time_influence
 var score_per_time = 5;
-var timer = 10;
+var timer = 15;
+var max_time = 60;
+
+var progress = 100;
+var step = progress / max_time;
+
 var time_right = 4;
 var time_wrong_factor = 0.5;
 
@@ -189,10 +194,15 @@ function init_game(){
 
     document.getElementById('game_div').appendChild(result_line);
 
+    var score_line = document.createElement('p');
+    score_line.id='score_line';
+    score_line.innerHTML="Punkte: <span id='score_a'>"+0+"</span>";
+    game_div.appendChild(score_line);
+
     var game_line = document.createElement('h1');
     game_line.id = 'game_line';
     game_line.className = "game-elements";
-    game_line.innerHTML = "<span id='z1'></span><span id='operator'> + </span><span id='z2'></span>";
+    game_line.innerHTML = "<span id='z1'></span><span id='operator'> + </span><span id='z2'></span> = ";
 
     game_div.appendChild(game_line);
 
@@ -204,14 +214,34 @@ function init_game(){
     user_tip.placeholder = "Hier kommt das Ergebnis rein";
     user_tip.onkeydown = clean;
     user_tip.onkeyup = clean;
-    game_div.appendChild(user_tip);
+    game_line.appendChild(user_tip);
     user_tip.focus();
 
+    var progress_bar = document.createElement('div');
+    progress_bar.id='progress_bar';
+    var bar = document.createElement('div');
+    bar.id='bar';
+    var percent = document.createElement('div');
+    percent.id='percent';
+
+
+    bar.appendChild(percent);
+    progress_bar.appendChild(bar);
+    game_div.appendChild(progress_bar);
+
+    document.getElementById('bar').style.width = progress.toFixed(1) + "%";
+    document.getElementById('percent').innerHTML = "<span id='sw_min'></span>:<span id='sw_s'></span>";
+
+
+
+    /*
     var stop_watch = document.createElement('h2');
     stop_watch.id='stop_watch';
     stop_watch.className = "game-elements";
     stop_watch.innerHTML = "<span id='sw_min'>0</span>:<span id='sw_s'>"+timer+"</span>";
     game_div.appendChild(stop_watch);
+    */
+
 
     var stat_table = document.createElement('table');
     stat_table.id='stat_table';
@@ -520,8 +550,10 @@ function clean(){
 function score_control(){
     if(score < 0){
         document.getElementById('score').innerHTML = 0;
+        document.getElementById('score_a').innerHTML = 0;
     }else{
         document.getElementById('score').innerHTML = score;
+        document.getElementById('score_a').innerHTML = score;
     }
 }
 
@@ -548,6 +580,13 @@ function game_timer(){
         }else{
             document.getElementById('sw_s').innerHTML = "" + sec;
         }
+
+        progress = actual_time_left * step;
+
+        //balkenfüllung
+        document.getElementById('bar').style.width = progress.toFixed(3) + "%";
+        //Prozentanzeige + nur theoretisch bitte nicht einkommentieren!!
+        //document.getElementById('percent').innerHTML = progress.toFixed(3) + "%";
 
         actual_time_left = actual_time_left -1;
         setTimeout('game_timer()', 1000);
