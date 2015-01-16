ba17d5d158c01f6ca032dd8c85c6ccfca50298fb
neufmanu <manu.neufeld@gmail.com>
2015-01-16 10:30:45 +0100

    Ajax trophy requst on games

--- public/javascripts/postHighscore.js
+++ public/javascripts/postHighscore.js
@@ -3,6 +3,11 @@
  */
 
 
+var trophy = document.createElement("div");
+
+var end_game_div = document.getElementById("end_game_div");
+
+
 function postScore(score, g_id){
     $.ajax({
         url: "/score",
@@ -12,11 +17,21 @@ function postScore(score, g_id){
             g_id: g_id
         },
         cache: false,
-        success: function () {
+        success: function (data) {
             console.log("Highscore posted");
+            if(data.pod =! 0){
+                console.log("got trophy");
+                trophy.id = "trophyWon";
+            }else{
+                trophy.id = "noTrophy";
+                console.log("got no trophy");
+            }
         },
         error: function () {
             console.log("Highscore not posted");
+            trophy.id = "noTrophy";
         }
-    })
+    });
+
+    end_game_div.appendChild(trophy);
 }
